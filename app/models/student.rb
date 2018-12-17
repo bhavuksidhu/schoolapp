1fc9f86890efe4714d047df6f1f6fee5b7a7d773
Mohd Sameer <mohdsameer1310@gmail.com>
2018-12-17 11:03:37 +0530

    upload

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < User
   has_one :student_detail
+  has_many :student_attachments
   accepts_nested_attributes_for :student_detail
-  # belongs_to :creator, class_name: User.to_s, foreign_key: "created_by_id"
-  # belongs_to :admin, class_name: User.to_s, foreign_key: "created_by_id"
+  accepts_nested_attributes_for :student_attachments
 end
