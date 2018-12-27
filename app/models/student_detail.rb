534d79d0080c80b3c9670a9b7fb163dd40d6bcda
Mohd Sameer <mohdsameer1310@gmail.com>
2018-12-27 16:54:59 +0530

    fee

--- app/models/student_detail.rb
+++ app/models/student_detail.rb
@@ -1,4 +1,5 @@
 class StudentDetail < ApplicationRecord
   belongs_to :standard
   belongs_to :student
+  enum fee_paid: {April: 1, May: 2, June: 3, July: 4,  August: 5, September: 6,  October: 7, November: 8,  December: 9, January: 10,  February: 11,  March: 12}
 end
