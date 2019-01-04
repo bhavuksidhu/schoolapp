cae629f9166229a965d5c557cea10393db087eb4
Mohd Sameer Malik <mohdsameer1310@gmail.com>
2019-01-04 16:29:04 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,9 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "school_app"
+set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
-set :deploy_to, '/var/www/my-app-name'
-set :use_sudo, true
-set :branch, 'master'
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
