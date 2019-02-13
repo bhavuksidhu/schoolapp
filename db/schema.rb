15fbe68b68c24e6fbcd84d511fb23cf8c82557f7
Mohd Sameer Malik <mohdsameer1310@gmail.com>
2019-02-13 13:00:01 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190107064933) do
+ActiveRecord::Schema.define(version: 20190204054248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20190107064933) do
     t.datetime "updated_at", null: false
     t.integer "student_id"
     t.integer "fee_paid"
+    t.text "about"
+    t.string "section"
     t.index ["standard_id"], name: "index_student_details_on_standard_id"
   end
 
